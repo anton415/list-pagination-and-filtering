87fe8552aa465c49cc2b72d62575946e9089e531
Anton Serdyuchenko <anton415@gmail.com>
2019-06-03 16:22:53 +0300

    Create global variables.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Developer: Anton Serdyuchenko anton415@gmail.com
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Variable to store the student list item elements in the student list.
+var studentListItem;
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
+// Variable to store the number of items to show on each "page".
+var numberOfItemsOnPage = 10;
 
 
 /*** 
